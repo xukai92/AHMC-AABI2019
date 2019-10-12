4d9160f765bd12a52ca24b682417a6e72f71a40e
Kai Xu <xukai921110@gmail.com>
2019-10-12 13:30:33 +0100

    Initial Overleaf Import

---
+++ example.jl
@@ -0,0 +1,13 @@
+using AdvancedHMC
+n_samples, n_adapts, target = 10_000, 2_000, 0.8 # set up sampling parameters
+q0 = randn(D)   # draw a random starting point
+### Building up NUTS
+metric = DiagEuclideanMetric(D) # diagonal Euclidean metric space
+h = Hamiltonian(metric, logdensity_f, grad_f)   # Hamiltonian on the target distribution
+eps_init = find_good_eps(h, q0) # initial step size
+int = Leapfrog(eps_init)    # leapfrog integrator
+traj = NUTS{Multinomial,GeneralisedNoUTurn}(int)    # multinomial sampling with generalised no U-turn   
+adaptor = StanHMCAdaptor(   # Stan's windowed adaptor
+    n_adapts, Preconditioner(metric), NesterovDualAveraging(target, eps_init)
+)
+samples, stats = sample(h, traj, q0, n_samples, adaptor, n_adapts)  # draw samples
